397bcb5e6bf87bb9bbea46be1df364ec679973fc
Kevin Swiber <kswiber@gmail.com>
2023-12-01 11:26:35 -0800

    Removing wasm-pack from JavaScript and Web, upgrading to Webpack 5, upgrading wasm-bindgen to latest.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,35 +1,37 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const dist = path.resolve(__dirname, 'dist');
+const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
   entry: {
-    index: './js/index.js',
+    index: "./js/index.js",
   },
   output: {
     path: dist,
-    filename: '[name].js',
+    filename: "[name].js",
+    clean: true,
   },
   devServer: {
-    contentBase: dist,
+    static: {
+      directory: dist,
+    },
+    compress: true,
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([path.resolve(__dirname, 'static')]),
-    new WasmPackPlugin({
-      crateDirectory: path.resolve(__dirname, '..'),
-      outDir: path.resolve(__dirname, 'wasm'),
+    new CopyWebpackPlugin({
+      patterns: [path.resolve(__dirname, "static")],
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
   },
 };
